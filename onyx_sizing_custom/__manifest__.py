9843c92989b4f2cf4c1c0e750328a3fb95c0ab9e
Yorhman Arango Ospina <76013740+tacticaweb@users.noreply.github.com>
2021-02-27 08:54:17 -0500

    Add files via upload

---
+++ onyx_sizing_custom/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Onyx Armour - Sizing',
+    'description': "Modulo de tallaje",
+    'author': "SOLUCIONES OPEN SOURCE",
+    'website': "http://www.solucionesos.com",
+    'summary': "Onyx - Agent Sizing",
+    'version': '0.1',
+    "license": "OPL-1",
+    'support': 'luis.varon@tacticaweb.com.co',
+    'category': 'module_category_project',
+        # any module necessary for this one to work correctly
+    'depends': ['sale', 'sale_stock','account'],
+
+    # always loaded
+    'data': [
+             'views/views.xml',
+             'security/ir.model.access.csv',
+            ],
+    'qweb': [
+               
+            ],
+    #"external_dependencies": {"python" : ["pytesseract"]},
+    'installable': True,
+}
