4180d4682a3c01623afac514ba28bac279c547ef
Yorhman Arango Ospina <76013740+tacticaweb@users.noreply.github.com>
2021-04-22 16:40:17 -0500

    Add files via upload

--- onyx_sizing_v2_custom/models/models.py
+++ onyx_sizing_v2_custom/models/models.py
@@ -33,7 +33,8 @@ class ResSizing(models.Model):
     agent = fields.Many2one('res.partner', string="Officer")
     sale_representative = fields.Many2one('res.users', string="Sale Representative")
     date = fields.Date(string='Date', default=fields.Date.today())
-    height = fields.Float(string='Height')
+    height_ft = fields.Float(string='Height Ft')
+    height_in = fields.Float(string='Height Inches')
     weight = fields.Float(string='Weight')
     chest_measure = fields.Float(string='Chest')
     abdomen_measure = fields.Float(string='Abdomen')
@@ -62,7 +63,7 @@ class ResSizing(models.Model):
             ('5XL', '5XL'),
             ('6XL', '6XL'),
         ],
-        string='Front Size', required=True,
+        string='Size Front', required=True,
         default='MD')
     size_front_length = fields.Selection([
             ('-2 inch', '-2 inch'),
@@ -101,7 +102,7 @@ class ResSizing(models.Model):
             ('5XL', '5XL'),
             ('6XL', '6XL'),
         ],
-        string='Back Size', required=True,
+        string='Size Back', required=True,
         default='MD')
     size_back_length = fields.Selection([
             ('-2 inch', '-2 inch'),
@@ -157,6 +158,7 @@ class ResSizing(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
+    #agent = fields.Many2one('res.partner', string="Officer", domain="[('is_agency','=',False)]")
     psnum = fields.Char(string='PSNUM')
         
     def _prepare_invoice_line(self, **optional_values):
@@ -169,6 +171,7 @@ class SaleOrderLine(models.Model):
         res = {
             'display_type': self.display_type,
             'name': self.name,
+            #'agent': self.agent,
             'psnum': self.psnum,
             'product_id': self.product_id.id,
             'product_uom_id': self.product_uom.id,
@@ -229,6 +232,7 @@ class SaleOrderLine(models.Model):
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
     
+    #agent = fields.Many2one('res.partner', string="Officer", domain="[('is_agency','=',False)]")
     psnum = fields.Char(string='PSNUM')
         
 class ProcurementGroup(models.Model):
