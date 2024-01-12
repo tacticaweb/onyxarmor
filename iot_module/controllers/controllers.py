8613971b2f52e2b2dab9dba051934b9e892aed46
Leyder Gallego <52385167+leygallego@users.noreply.github.com>
2024-01-12 09:42:23 -0500

    Add files via upload

--- iot_module/controllers/controllers.py
+++ iot_module/controllers/controllers.py
@@ -1,67 +1,96 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
-
-
-# class TacticaPrototipo(http.Controller):
-#     @http.route('/tactica_prototipo/tactica_prototipo', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/tactica_prototipo/tactica_prototipo/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('tactica_prototipo.listing', {
-#             'root': '/tactica_prototipo/tactica_prototipo',
-#             'objects': http.request.env['tactica_prototipo.tactica_prototipo'].search([]),
-#         })
-
-#     @http.route('/tactica_prototipo/tactica_prototipo/objects/<model("tactica_prototipo.tactica_prototipo"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('tactica_prototipo.object', {
-#             'object': obj
-#         })
-
-
-
+import logging
 from odoo import http
 from odoo.http import request
 from odoo.exceptions import UserError
+import json
+
+_logger = logging.getLogger(__name__)
 
 class TacticaPrototipoController(http.Controller):
     @http.route('/iot/api', type='json', auth="public", methods=['POST'], csrf=False)
-    def receive_data(self):
+    def receive_data(self, **kwargs):
         try:
             # Obtener datos JSON directamente de la solicitud
             json_data = request.jsonrequest
 
-            print("Veré Recibiendo datos desde Postman:", json_data)
+            if isinstance(json_data, str):
+                # Si json_data es una cadena, intenta cargarla como JSON
+                json_data = json.loads(json_data)
 
-            received_csn = json_data.get('csn')
-            received_etapa = json_data.get('etapa')
 
-            print(f"Veré CSN recibido: {received_csn}")
-            print(f"Veré Etapa recibida: {received_etapa}")
+            # Validar si los datos son una lista o un solo elemento
+            if isinstance(json_data, (list, dict)):
+                # Si se recibe una lista de elementos JSON o un solo elemento JSON
+                return self.process_data_list(json_data)
 
-            # Validación de datos
-            if not received_csn or not received_etapa:
-                raise UserError("Los campos 'csn' y 'etapa' son estrictamente obligatorios.")
+            raise UserError("Formato de datos no válido.")
 
-            RFIDModel = request.env['rfid.data']
-            new_rfid_data = RFIDModel.create({
-                'csn': received_csn,
-                'etapa': received_etapa,
-            })
-
-            print("Veré Registro creado exitosamente:", new_rfid_data)
-
-            return {'status': 'OK', 'message': 'Data received Ok.'}
         except UserError as e:
-            print(f"Veré Error de usuario: {e}")
-            return {'status': 'Error', 'message': str(e)}
+            _logger.error("Error de usuario: %s", e)
+            return {'status': 'Error', 'message': str(e)}, 400  # Bad Request
         except Exception as e:
-            print(f"Veré Error inesperado: {e}")
-            return {'status': 'Error', 'message': 'Se produjo un error inesperado.'}
+            _logger.exception("Error inesperado: %s", e)
+            return {'status': 'Error', 'message': 'Se produjo un error inesperado.'}, 500  # Internal Server Error
+
+    def process_data_list(self, json_data_list):
+        RFIDModel = request.env['rfid.data']
+
+        # Verifica si json_data_list es un diccionario y si contiene la clave 'json_data_list'
+        if isinstance(json_data_list, dict) and 'json_data_list' in json_data_list:
+            json_data_list = json_data_list['json_data_list']  # Obtén la lista de elementos
+
+            # Asegúrate de que json_data_list sea realmente una lista
+            if isinstance(json_data_list, list):
+                # Itera sobre los elementos de la lista
+                for index, data in enumerate(json_data_list):
+                    _logger.info("Processing item at index %s: %s", index, data)
+
+                    # Verifica si el elemento es un diccionario
+                    if isinstance(data, dict):
+                        received_csn = data.get('csn')
+                        received_etapa = data.get('etapa')
+
+                        _logger.info("CSN received: %s", received_csn)
+                        _logger.info("Received stage: %s", received_etapa)
+
+                        # Validación de datos
+                        _logger.info("Validating data for item at index %s", index)
+                        if not received_csn or not received_etapa:
+                            raise UserError("Fields 'csn' and 'etapa' are mandatory.")
+                        _logger.info("Data validation successful for item at index %s", index)
+
+                        # Validar que la clave CSN sea única
+                        existing_rfid_data = RFIDModel.search([('csn', '=', received_csn)], limit=1)
+                        _logger.info("Search results: %s", existing_rfid_data)
+
+                        if existing_rfid_data:
+                            # If the CSN already exists, update the existing record
+                            existing_rfid_data.write({
+                                'etapa': received_etapa,
+                            })
+                            _logger.info("Record updated successfully: %s", existing_rfid_data)
+                        else:
+                            # If the CSN doesn't exist, create a new record
+                            new_rfid_data = RFIDModel.create({
+                                'csn': received_csn,
+                                'etapa': received_etapa,
+                            })
+
+                            _logger.info("Record created successfully: %s", new_rfid_data)
+
+                    else:
+                        raise UserError("Invalid data format. Expected a list of items (dictionaries).")
+
+                _logger.info("End of loop for json_data_list.")
+                return {'status': 'OK', 'message': 'Data received successfully.'}
+            else:
+                raise UserError("Invalid data format. Expected a list of items.")
+        else:
+            raise UserError("Invalid data format. 'json_data_list' key not found.")
 
 
+
+#############################
 
 
     
@@ -69,6 +98,7 @@ class TacticaPrototipoController(http.Controller):
     def index(self, **kw):
         try:
             # Consulta todas las órdenes de venta
+            _logger.info("Ingresando al endpoint /iot/sale")
             sales_orders = http.request.env['sale.order'].search([])
 
         except Exception as e:
@@ -88,6 +118,7 @@ class TacticaPrototipoController(http.Controller):
     def show_contacts(self, **kw):
         try:
             # Consulta todos los contactos
+            _logger.info("Ingresando al endpoint /iot/contacts")
             contacts = http.request.env['res.partner'].search([])
 
         except Exception as e:
@@ -103,27 +134,6 @@ class TacticaPrototipoController(http.Controller):
         contact_output += "</ul>"
         return contact_output
         
-        # return http.request.render('odoo_controller.index', {
-        #      "sales": sales_orders,
-        #  })
-
-    # @http.route('/contactos/<model("contact.template"):contact>', auth="public")
-    # def contact_test(self, contact):
-    #     return http.request.render('tactica_prototipo.contact', {
-    #         "contact": contact
-    #     })
-
-    
-    # @http.route('/tactica_prototipo/get_partners', type='json', auth="public", methods=['GET'])
-    # def get_partners(self):
-    #     # Recuperar los nombres de los registros de res.partner
-    #     ContactModel = request.env['res.partner']
-    #     partners = ContactModel.search([])
-
-    #     # Extraer los nombres de los registros
-    #     partner_names = partners.mapped('name')
-
-    #     return {'partner_names': partner_names}
-    
+       
 
    
